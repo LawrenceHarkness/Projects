a76a65c7410fe6e4b62b3de945932f0d790ea9c4
LawrenceHarkness <lawrenceharkness3@gmail.com>
2020-10-01 11:21:36 +0100

    Added binary and linear search to name list

--- Python/Sorting&Searching/linear_search.py
+++ Python/Sorting&Searching/linear_search.py
@@ -1,13 +1,65 @@
-Array = ["James", "Joe", "Sam", "Gerald", "Cory"]
-NSF = False
-NS = input("Enername")
+import time
 
-for i in Array:
-    if i == NS:
-        print("found")
-        NSF = True
-if NSF == False:
-    print("Notfound adding name")
+
+def BinarySearch(Array,NS):
+    Array.sort()
+    High = len(Array) - 1
+    Low = 0
+    Mid = 0
+    while Low <= High:
+        Mid = (High + Low) // 2
+        if Array[Mid] < NS:
+            Low = Mid + 1
+        elif Array[Mid] > NS:
+            High = Mid - 1
+        else:
+        #elif Array[Mid] == NS:
+            print("Found")
+            return(Array)
     Array.append(NS)
+    print("Value Not Found")
+    return(Array)
+def linearSearch(Array,NS):
 
+    NSF = False
+
+
+    for i in Array:
+        if i == NS:
+            print("found")
+            NSF = True
+    if NSF == False:
+        print("Not found adding name")
+        Array.append(NS)
+    return(Array)
+
+
+
+def writeN(Array,FN):
+    with open(FN,mode='w') as f:
+        f.write('\n'.join(Array))
+
+
+
+def file_read(fname):
+    content_array = []
+    with open(fname) as f:
+        # Content_list is the list that contains the read lines.
+        for line in f:
+            content_array.append(line.replace('\n',''))
+
+
+    return content_array
+
+Array = file_read('list.txt')
+NS = input("Enter name")
+start_time = time.time()
+Array = linearSearch(Array,NS)
+print("--- Linear %s seconds ---" % (time.time() - start_time))
+
+start_time = time.time()
+Array = BinarySearch(Array,NS)
+print("--- Binary %s seconds ---" % (time.time() - start_time))
+
+writeN(Array,"list.txt")
 
